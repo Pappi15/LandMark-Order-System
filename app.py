0c08a82df3ea815efc62107800c24778775a5b67
Pappi15 <122734545+Pappi15@users.noreply.github.com>
2023-04-15 01:50:12 +0100

    Template made

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Validate the username and password here
+        return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
+
